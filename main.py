6892cb751d93cb46308fbd9636fa2b5d231f5146
zmdiv <66516043+zmdiv@users.noreply.github.com>
2022-08-27 19:42:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,15 +3,18 @@ import pdfplumber
 from art import tprint
 from pathlib import Path
 
-def pdf_to_mp3(file_path= 'test.pdf', language= 'en'):
+# checking if the file to work with exists
+def pdf_to_mp3(file_path= 'test.pdf', language= 'en'): 
     if Path(file_path).is_file() and Path(file_path).suffix == '.pdf':
         return 'File exists'
-
+    
+# extracting text from every page of the pdf file and cleaning it
         with pdfplumber.PDF(open(file=file_path, mode='rb')) as pdf:
             pages = [page.extract_text() for page in pdf.pages]
         text = ''.join(pages)
         text = text.replace('\n', '')
 
+# creating .mp3 file from the text. indicating language and pace. 
         my_audio = gTTS(text=text, lang=language, slow=False)
         file_name = Path(file_path).stem
         my_audio.save(f'{file_name}.mp3')
@@ -19,7 +22,8 @@ def pdf_to_mp3(file_path= 'test.pdf', language= 'en'):
         return f'{file_name}.mp3 saved succesfully'
     else:
         return 'File does not exist, please check again'
-
+    
+#  accepting inputs and calling above created function in the main functions. 
 def main():
     tprint('PDF>>TO>>MP3', font='bulbhead')
     file_path = input('enter the file path: ')
